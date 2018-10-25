8329efef404527c37446be55c703b37145d6dcea
James Njiru <james.kithu2@gmail.com>
2018-10-25 11:02:24 +0300

    initial commit after cloning ah-codeofduty

---
+++ authors/apps/authentication/backends.py
@@ -0,0 +1,17 @@
+# import jwt
+#
+# from django.conf import settings
+#
+# from rest_framework import authentication, exceptions
+#
+# from .models import User
+
+"""Configure JWT Here"""
+
+
+class JWTAuthentication:
+    def authenticate(self, argument2):
+        pass
+
+    def authenticate_header(self, argument2):
+        pass
